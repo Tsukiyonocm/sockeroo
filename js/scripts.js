25d4c934c3a290a0904356011860676d10d169d7
Tsukiyonocm <tsukiyonocm@yahoo.com>
2018-12-12 12:20:52 -0500

    added javascript, fixed scroll icon

--- js/scripts.js
+++ js/scripts.js
@@ -9,3 +9,31 @@ $("a[href^='#'").on("click", function(e){
     }, 1000);
 })
 
+
+let section = document.querySelectorAll(".scroll");
+let sections = {};
+let threshold = 30;
+
+section.forEach(function(e){
+    // console.log(e.id, e.offsetTop);
+    sections[e.id] = e.offsetTop;
+})
+
+window.onscroll = function(){
+    let scrollPos = document.documentElement.scrollTop || document.body.scrollTop;
+    for (let i in sections){
+        if(sections[i] <= scrollPos + threshold){
+            document.querySelector(".active").setAttribute("class", "nav-link");
+            document.querySelector("a[href*="+i+"]").setAttribute("class", "active nav-link");
+        }
+    }
+}
+
+$(window).scroll(function(){
+    if($(window).scrollTop() + $(window).height() > $(document).height() - 200){
+        $("#scroll_down").hide();
+    }
+    else {
+        $("#scroll_down").show();
+    }
+})
